4545cd963b9b3da45326d0955c7a7d4633d29bf9
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-03-02 19:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine) {
 function currentLine(line) {
   var realTime = [];
   if (line.length === 0){
-    return `The line is currenty empty.`;
+    return `The line is currently empty.`;
   } else {
     for (let i = 0; i < line.length; i++) {
       realTime.push(` ${i + 1}. ${line[i]}`);
